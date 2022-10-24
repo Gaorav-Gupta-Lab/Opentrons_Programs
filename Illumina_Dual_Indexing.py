6327bf1bac57d06e3330cdf2f686db60f10da4c5
Dennis Simpson <g2.checkpoint@gmail.com>
2022-10-24 14:15:46 -0400

    Issue Fix #16.  v1.0.1

--- Illumina_Dual_Indexing.py
+++ Illumina_Dual_Indexing.py
@@ -2,6 +2,8 @@ import datetime
 import os
 import platform
 import sys
+import csv
+from types import SimpleNamespace
 from collections import defaultdict
 from contextlib import suppress
 from opentrons import protocol_api
@@ -19,11 +21,101 @@ import Utilities
 
 # metadata
 metadata = {
-    'protocolName': 'Illumina Dual Indexing v1.0.0',
+    'protocolName': 'Illumina Dual Indexing v1.0.1',
     'author': 'Dennis Simpson',
     'description': 'Add Illumina dual indexing to library',
     'apiLevel': '2.13'
 }
+
+
+def parse_sample_template(input_file):
+    """
+    Parse the TSV file and return data objects to run def.
+    @param input_file:
+    @return:
+    """
+    line_num = 0
+    options_dictionary = defaultdict(str)
+    sample_dictionary = defaultdict(list)
+    index_file = list(csv.reader(open(input_file), delimiter='\t'))
+    for line in index_file:
+        if line_num == 0:
+            options_dictionary["Version"] = line[1]
+            options_dictionary["Template"] = line[0].strip("#")
+        line_num += 1
+        col_count = len(line)
+        tmp_line = []
+        sample_key = ""
+        if col_count > 0 and "#" not in line[0] and len(line[0].split("#")[0]) > 0:
+            # Skip any lines that are blank or comments.
+            for i in range(7):
+                try:
+                    line[i] = line[i].split("#")[0]  # Strip out end of line comments.
+                except IndexError:
+                    continue
+
+                if i == 0 and "--" in line[0]:
+                    key = line[0].strip('--')
+                    key_value = line[1]
+                    if "Target_" in key or "PositiveControl_" in key:
+                        key_value = (line[1], line[2], line[3])
+                    options_dictionary[key] = key_value
+                elif "--" not in line[0] and int(line[0]) < 12:
+                    sample_key = line[0], line[1]
+                    tmp_line.append(line[i])
+            if sample_key:
+                sample_dictionary[sample_key] = tmp_line
+    return sample_dictionary, SimpleNamespace(**options_dictionary)
+
+
+def labware_parsing(args, ctx):
+    # Extract Slot information
+    slot_list = ["Slot1", "Slot2", "Slot3", "Slot4", "Slot5", "Slot6", "Slot7", "Slot8", "Slot9", "Slot10", "Slot11"]
+    labware_dict = {}
+    slot_dict = {}
+    tipbox_dict = \
+        {"p10_multi": "opentrons_96_tiprack_10ul", "p10_single": "opentrons_96_tiprack_10ul",
+         "p20_single_gen2": ["opentrons_96_tiprack_20ul", "opentrons_96_filtertiprack_20ul"],
+         "p300_single_gen2": ["opentrons_96_tiprack_300ul", "opentrons_96_filtertiprack_300ul"]}
+    # Pipette Tip Boxes
+    left_tiprack_list = []
+    right_tiprack_list = []
+    for i in range(len(slot_list)):
+        labware = getattr(args, "{}".format(slot_list[i]))
+        if labware:
+            slot_dict[str(i + 1)] = labware
+            labware_dict[str(i + 1)] = ctx.load_labware(labware, str(i + 1))
+            if labware in tipbox_dict[args.LeftPipette]:
+                left_tiprack_list.append(labware_dict[str(i + 1)])
+            elif labware in tipbox_dict[args.RightPipette]:
+                right_tiprack_list.append(labware_dict[str(i + 1)])
+
+    return labware_dict, slot_dict, left_tiprack_list, right_tiprack_list
+
+
+def plate_layout(labware):
+    """
+    Define the destination layout for the reactions.  Can be 96-well plate or 8-well strip tubes
+    @param labware:
+    @return:
+    """
+
+    layout_data = defaultdict(list)
+    for k in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']:
+        layout_data[k] = ['', '', '', '', '', '', '', '', '', '', '', '', ]
+
+    if labware == "stacked_96_well":
+        column_index = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    elif labware == "8_well_strip_tubes_200ul":
+        column_index = [1, 3, 5, 7, 9, 11, 12]
+
+    rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+    plate_layout_by_column = []
+    for i in column_index:
+        for row in rows:
+            plate_layout_by_column.append("{}{}".format(row, i))
+
+    return plate_layout_by_column, layout_data
 
 
 def add_pcr_mix(args, labware_dict, sample_dest_dict, left_pipette, right_pipette):
@@ -161,13 +253,6 @@ def dispense_samples(args, sample_parameters, labware_dict, left_pipette, right_
 
 
 def run(ctx: protocol_api.ProtocolContext):
-    ctx.comment("Begin {}".format(metadata['protocolName']))
-
-    # Turn on rail lights and pause program so user can load robot deck.
-    # ctx.set_rail_lights(True)
-    # ctx.pause("Load Labware onto robot deck and click resume when ready to continue")
-    # ctx.home()
-    ctx.set_rail_lights(False)
 
     # TSV file location on OT-2
     tsv_file_path = "{0}var{0}lib{0}jupyter{0}notebooks{0}ProcedureFile.tsv".format(os.sep)
@@ -175,8 +260,18 @@ def run(ctx: protocol_api.ProtocolContext):
         # Temp TSV file location on Win10 Computers for simulation
         tsv_file_path = "C:{0}Users{0}{1}{0}Documents{0}TempTSV.tsv".format(os.sep, os.getlogin())
 
-    sample_parameters, args = Utilities.parse_sample_template(tsv_file_path)
-    labware_dict, slot_dict, left_tiprack_list, right_tiprack_list = Utilities.labware_parsing(args, ctx)
+    sample_parameters, args = parse_sample_template(tsv_file_path)
+
+    ctx.comment("Begin {}".format(metadata['protocolName']))
+
+    # Turn on rail lights and pause program so user can load robot deck.
+    # ctx.set_rail_lights(True)
+    # ctx.pause("Load Labware onto robot deck and click resume when ready to continue")
+    # ctx.home()
+
+    ctx.set_rail_lights(False)
+
+    labware_dict, slot_dict, left_tiprack_list, right_tiprack_list = labware_parsing(args, ctx)
 
     # Pipettes
     left_pipette = ctx.load_instrument(args.LeftPipette, 'left', tip_racks=left_tiprack_list)
--- Utilities.py
+++ Utilities.py
@@ -8,38 +8,9 @@ Chapel Hill NC, 27599
 @copyright 2022
 
 """
-import csv
 import math
-import os
-from collections import defaultdict
-from types import SimpleNamespace
 
-__version__ = "1.0.0"
-
-
-def plate_layout(labware):
-    """
-    Define the destination layout for the reactions.  Can be 96-well plate or 8-well strip tubes
-    @param labware:
-    @return:
-    """
-
-    layout_data = defaultdict(list)
-    for k in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']:
-        layout_data[k] = ['', '', '', '', '', '', '', '', '', '', '', '', ]
-
-    if labware == "stacked_96_well":
-        column_index = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-    elif labware == "8_well_strip_tubes_200ul":
-        column_index = [1, 3, 5, 7, 9, 11, 12]
-
-    rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
-    plate_layout_by_column = []
-    for i in column_index:
-        for row in rows:
-            plate_layout_by_column.append("{}{}".format(row, i))
-
-    return plate_layout_by_column, layout_data
+__version__ = "1.0.1"
 
 
 def labware_cone_volume(args, labware_name):
@@ -81,95 +52,6 @@ def res_tip_height(res_vol, well_dia, cone_vol, bottom_offset):
         height = bottom_offset
 
     return round(height, 1)
-
-
-def parse_sample_template(input_file):
-    """
-    Parse the TSV file and return data objects to run def.
-    @param input_file:
-    @return:
-    """
-    line_num = 0
-    options_dictionary = defaultdict(str)
-    sample_dictionary = defaultdict(list)
-    index_file = list(csv.reader(open(input_file), delimiter='\t'))
-    for line in index_file:
-        if line_num == 0:
-            options_dictionary["Version"] = line[1]
-            options_dictionary["Template"] = line[0].strip("#")
-        line_num += 1
-        col_count = len(line)
-        tmp_line = []
-        sample_key = ""
-        if col_count > 0 and "#" not in line[0] and len(line[0].split("#")[0]) > 0:
-            # Skip any lines that are blank or comments.
-            for i in range(7):
-                try:
-                    line[i] = line[i].split("#")[0]  # Strip out end of line comments.
-                except IndexError:
-                    continue
-
-                if i == 0 and "--" in line[0]:
-                    key = line[0].strip('--')
-                    key_value = line[1]
-                    if "Target_" in key or "PositiveControl_" in key:
-                        key_value = (line[1], line[2], line[3])
-                    options_dictionary[key] = key_value
-                elif "--" not in line[0] and int(line[0]) < 12:
-                    sample_key = line[0], line[1]
-                    tmp_line.append(line[i])
-            if sample_key:
-                sample_dictionary[sample_key] = tmp_line
-    return sample_dictionary, SimpleNamespace(**options_dictionary)
-
-
-def initialize_system(ctx):
-    # TSV file location on OT-2
-    tsv_file_path = "{0}var{0}lib{0}jupyter{0}notebooks{0}ProcedureFile.tsv".format(os.sep)
-    if not os.path.isfile(tsv_file_path):
-        # Temp TSV file location on Win10 Computers for simulation
-        tsv_file_path = "C:{0}Users{0}{1}{0}Documents{0}TempTSV.tsv".format(os.sep, os.getlogin())
-
-    sample_parameters, args = parse_sample_template(tsv_file_path)
-    labware_dict, slot_dict, left_tiprack_list, right_tiprack_list = labware_parsing(args, ctx)
-
-    # Pipettes
-    left_pipette = ctx.load_instrument(args.LeftPipette, 'left', tip_racks=left_tiprack_list)
-    right_pipette = ctx.load_instrument(args.RightPipette, 'right', tip_racks=right_tiprack_list)
-
-    # Set the location of the first tip in box.
-    left_pipette.starting_tip = left_tiprack_list[0].wells_by_name()[args.LeftPipetteFirstTip.upper()]
-    right_pipette.starting_tip = right_tiprack_list[0].wells_by_name()[args.RightPipetteFirstTip.upper()]
-    return_list = \
-        [args, tsv_file_path, sample_parameters, labware_dict, left_tiprack_list, right_tiprack_list, left_pipette,
-         right_pipette, left_pipette.starting_tip, right_pipette.starting_tip, slot_dict]
-
-    return return_list
-
-
-def labware_parsing(args, ctx):
-    # Extract Slot information
-    slot_list = ["Slot1", "Slot2", "Slot3", "Slot4", "Slot5", "Slot6", "Slot7", "Slot8", "Slot9", "Slot10", "Slot11"]
-    labware_dict = {}
-    slot_dict = {}
-    tipbox_dict = \
-        {"p10_multi": "opentrons_96_tiprack_10ul", "p10_single": "opentrons_96_tiprack_10ul",
-         "p20_single_gen2": ["opentrons_96_tiprack_20ul", "opentrons_96_filtertiprack_20ul"],
-         "p300_single_gen2": ["opentrons_96_tiprack_300ul", "opentrons_96_filtertiprack_300ul"]}
-    # Pipette Tip Boxes
-    left_tiprack_list = []
-    right_tiprack_list = []
-    for i in range(len(slot_list)):
-        labware = getattr(args, "{}".format(slot_list[i]))
-        if labware:
-            slot_dict[str(i + 1)] = labware
-            labware_dict[str(i + 1)] = ctx.load_labware(labware, str(i + 1))
-            if labware in tipbox_dict[args.LeftPipette]:
-                left_tiprack_list.append(labware_dict[str(i + 1)])
-            elif labware in tipbox_dict[args.RightPipette]:
-                right_tiprack_list.append(labware_dict[str(i + 1)])
-
-    return labware_dict, slot_dict, left_tiprack_list, right_tiprack_list
 
 
 def load_tipracks(protocol, tiprack_list, labware_dict):
