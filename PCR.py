190b2dbd970e90b8329900de0715d86a394e63f2
Dennis Simpson <g2.checkpoint@gmail.com>
2023-03-08 09:35:44 -0500

    Forgot to add new labware definitions

--- PCR.py
+++ PCR.py
@@ -104,7 +104,9 @@ def plate_layout(labware):
     for k in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']:
         layout_data[k] = ['', '', '', '', '', '', '', '', '', '', '', '', ]
 
-    if labware == "stacked_96_well" or labware == "8_well_strip_dilution_tubes":
+    if labware == "stacked_96_well" or labware == "bigwell_96_tuberack_200ul_dilution_tube" \
+            or labware == "biorad_ddpcr_96_wellplate_100ul":
+
         column_index = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
     elif labware == "8_well_strip_tubes_200ul":
